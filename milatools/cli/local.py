9ac157e782da2402a7d47ff4cc85382dbd428636
Fabrice Normandin <normandf@mila.quebec>
2023-11-06 15:04:11 -0500

    Remove the ParamSpecs, make methods sigs explicit
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>

--- milatools/cli/local.py
+++ milatools/cli/local.py
@@ -2,56 +2,39 @@ from __future__ import annotations
 
 import subprocess
 from subprocess import CompletedProcess
-from typing import Callable, Iterable, Sequence
-
-from typing_extensions import Concatenate, ParamSpec
+from typing import IO, Any, Iterable, Sequence
 
 from .utils import CommandNotFoundError, T, shjoin
-
-P = ParamSpec("P")
-Args = Concatenate[Sequence[str], P]
 
 
 class Local:
     def display(self, args: Iterable[str]) -> None:
         print(T.bold_green("(local) $ ", shjoin(args)))
 
-    def silent_get(
-        self,
-        cmd: Sequence[str],
-        _check_output_fn: Callable[
-            Concatenate[str | Sequence[str], P], str
-        ] = subprocess.check_output,
-        *args: P.args,
-        **kwargs: P.kwargs,
-    ) -> str:
-        kwargs["universal_newlines"] = True
-        return _check_output_fn(cmd, *args, **kwargs)
+    def silent_get(self, cmd: Sequence[str]) -> str:
+        return subprocess.check_output(cmd, universal_newlines=True)
 
-    def get(
-        self,
-        cmd: Sequence[str],
-        _check_output_fn: Callable[
-            Concatenate[str | Sequence[str], P], str
-        ] = subprocess.check_output,
-        *args: P.args,
-        **kwargs: P.kwargs,
-    ):
+    def get(self, cmd: Sequence[str]) -> str:
         self.display(cmd)
-        kwargs["universal_newlines"] = True
-        return _check_output_fn(cmd, *args, **kwargs)
+        return subprocess.check_output(cmd, universal_newlines=True)
 
     def run(
         self,
         cmd: Sequence[str],
-        _run_fn: Callable[Args[P], CompletedProcess[str]] = subprocess.run,
-        *args: P.args,
-        **kwargs: P.kwargs,
+        *,
+        stdout: int | IO[Any] | None = None,
+        stderr: int | IO[Any] | None = None,
+        capture_output: bool = False,
     ) -> CompletedProcess[str]:
         self.display(cmd)
         try:
-            kwargs["universal_newlines"] = True
-            return _run_fn(cmd, *args, **kwargs)
+            return subprocess.run(
+                cmd,
+                stdout=stdout,
+                stderr=stderr,
+                capture_output=capture_output,
+                universal_newlines=True,
+            )
         except FileNotFoundError as e:
             if e.filename == cmd[0]:
                 raise CommandNotFoundError(e.filename) from e
@@ -60,13 +43,14 @@ class Local:
     def popen(
         self,
         cmd: Sequence[str],
-        _popen_fn: Callable[Args[P], subprocess.Popen] = subprocess.Popen,
-        *args: P.args,
-        **kwargs: P.kwargs,
+        *,
+        stdout: int | IO[Any] | None = None,
+        stderr: int | IO[Any] | None = None,
     ) -> subprocess.Popen:
         self.display(cmd)
-        kwargs["universal_newlines"] = True
-        return _popen_fn(cmd, *args, **kwargs)
+        return subprocess.Popen(
+            cmd, stdout=stdout, stderr=stderr, universal_newlines=True
+        )
 
     def check_passwordless(self, host: str) -> bool:
         results = self.run(
