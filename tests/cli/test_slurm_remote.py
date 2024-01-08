7ccd89e87d474e01c2cd5ca71c88f819772baca0
Fabrice Normandin <normandf@mila.quebec>
2024-01-08 15:32:08 -0500

    Trying to add tests using the local SLURM cluster
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>

--- tests/cli/test_slurm_remote.py
+++ tests/cli/test_slurm_remote.py
@@ -1,60 +1,76 @@
 from __future__ import annotations
 
 import os
+from logging import getLogger as get_logger
 
 import pytest
 
-from milatools.cli.remote import Remote
+from milatools.cli.remote import Remote, SlurmRemote
 
-# from fabric.connection import Connection
+from .common import requires_s_flag
 
-SLURM_CLUSTER = os.environ.get("SLURM_CLUSTER", "mila")
+logger = get_logger(__name__)
 
-CONN_TO_SLURM_CLUSTER_AVAILABLE = False
-try:
-    # connection = Connection(SLURM_CLUSTER)
-    # CONN_TO_SLURM_CLUSTER_AVAILABLE = connection.run("which srun").stdout != ""
-    CONN_TO_SLURM_CLUSTER_AVAILABLE = True
-except Exception:
-    pass
+SLURM_CLUSTER = os.environ.get("SLURM_CLUSTER")
 
 requires_access_to_slurm_cluster = pytest.mark.skipif(
-    not CONN_TO_SLURM_CLUSTER_AVAILABLE,
+    not SLURM_CLUSTER,
     reason="Requires ssh access to a SLURM cluster.",
 )
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def remote():
-    return Remote(hostname=SLURM_CLUSTER)
+    if not SLURM_CLUSTER:
+        pytest.skip("Requires ssh access to a SLURM cluster.")
+    return Remote(SLURM_CLUSTER)
 
 
-# @pytest.fixture
-# def slurm_remote(remote: Remote):
-#     job_name = "mila-tools"
-#     return SlurmRemote(
-#         connection=remote.connection,
-#         alloc=[
-#             "-J",
-#             job_name,
-#             "--nodes=1",
-#             "--ntasks=1",
-#             "--cpus-per-task=1",
-#             "--mem=1G",
-#             "--time=00:01:00",
-#         ],
-#     )
+@pytest.fixture
+def slurm_remote(remote: Remote):
+    job_name = "mila-tools"
+
+    accounts = remote.get_output(
+        "sacctmgr -n show associations where user=$USER format=Account%100"
+    ).split()
+    logger.info(f"Accounts on the slurm cluster {remote.hostname}: {accounts}")
+
+    assert accounts
+    account = accounts[0]
+    # Maybe select the account with highest priority?
+
+    return SlurmRemote(
+        connection=Remote,
+        alloc=[
+            "-J",
+            job_name,
+            f"--account={account}",
+            "--nodes=1",
+            "--ntasks=1",
+            "--cpus-per-task=1",
+            "--mem=1G",
+            "--time=00:01:00",
+        ],
+    )
 
 
-# @requires_access_to_slurm_cluster
-# def test_ensure_allocation(slurm_remote: SlurmRemote):
-#     # TODO: Really confusing: What exactly is SlurmRemote? Is it a connection to the
-#     # compute node? Or is it a connection to the login node?
-#     data, proc = slurm_remote.ensure_allocation()
-#     # {"node_name": node_name, "jobid": results["jobid"]}, proc
-#     print(data, proc)
+@requires_s_flag
+@requires_access_to_slurm_cluster
+def test_ensure_allocation(slurm_remote: SlurmRemote):
+    # TODO: The SlurmRemote class is quite confusing: Is it a connection to the compute
+    # node? Or is it a connection to the login node?
+
+    # Actually runs the `salloc` command. Unclear if the connection to the compute node
+    # is saved in `proc`..
+    data, proc = slurm_remote.ensure_allocation()
+
+    hostname = data["node_name"]
+
+    # {"node_name": node_name, "jobid": results["jobid"]}, proc
+    print(data, proc, hostname)
 
 
+@requires_s_flag
 @requires_access_to_slurm_cluster
 def test_can_launch_srun(remote: Remote):
     output = remote.get_output(
