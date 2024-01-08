dd225ada0de79543cd90a7134d60ddaa76dbbe07
Fabrice Normandin <normandf@mila.quebec>
2024-01-08 18:05:41 -0500

    `SlurmRemote.ensure_allocation` test works on Mila
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>

--- tests/cli/test_slurm_remote.py
+++ tests/cli/test_slurm_remote.py
@@ -1,22 +1,38 @@
+"""Tests that use an actual SLURM cluster.
+
+The cluster to use can be specified by setting the SLURM_CLUSTER environment variable.
+During the CI on GitHub, a small local slurm cluster is setup with a GitHub Action, and
+SLURM_CLUSTER is set to "localhost".
+"""
 from __future__ import annotations
 
+import functools
 import os
+import time
 from logging import getLogger as get_logger
 
+import fabric.runners
 import pytest
 
 from milatools.cli.remote import Remote, SlurmRemote
-
-from .common import requires_s_flag
+from milatools.cli.utils import T
 
 logger = get_logger(__name__)
 
 SLURM_CLUSTER = os.environ.get("SLURM_CLUSTER")
+JOB_NAME = "mila-tools"
+WCKEY = "milatools_test"
 
 requires_access_to_slurm_cluster = pytest.mark.skipif(
     not SLURM_CLUSTER,
     reason="Requires ssh access to a SLURM cluster.",
 )
+
+
+@pytest.fixture(scope="module", autouse=True)
+def cancel_all_milatools_jobs_after_tests(remote: Remote):
+    yield
+    remote.run(f"scancel -u $USER --wckey={WCKEY}")
 
 
 @pytest.fixture(scope="session")
@@ -26,55 +42,127 @@ def remote():
     return Remote(SLURM_CLUSTER)
 
 
-@pytest.fixture
-def slurm_remote(remote: Remote):
-    job_name = "mila-tools"
-
+@pytest.fixture(scope="session")
+def account(remote: Remote):
+    """Fixture that gets an account to be used for test jobs on the slurm cluster."""
     accounts = remote.get_output(
-        "sacctmgr -n show associations where user=$USER format=Account%100"
+        "sacctmgr --noheader show associations where user=$USER format=Account%100",
     ).split()
     logger.info(f"Accounts on the slurm cluster {remote.hostname}: {accounts}")
-
     assert accounts
     account = accounts[0]
-    # Maybe select the account with highest priority?
+    logger.info(f"Using account {account} for jobs launched in tests.")
+    return account
 
-    return SlurmRemote(
-        connection=Remote,
-        alloc=[
-            "-J",
-            job_name,
-            f"--account={account}",
-            "--nodes=1",
-            "--ntasks=1",
-            "--cpus-per-task=1",
-            "--mem=1G",
-            "--time=00:01:00",
-        ],
+
+@pytest.fixture(scope="session")
+def allocation_flags(account: str):
+    return (
+        f"--job-name={JOB_NAME} --wckey={WCKEY} --account={account} --nodes=1 "
+        f"--ntasks=1 --cpus-per-task=1 --mem=1G --time=00:01:00"
     )
 
 
-@requires_s_flag
 @requires_access_to_slurm_cluster
-def test_ensure_allocation(slurm_remote: SlurmRemote):
+def test_srun(remote: Remote, allocation_flags: str):
+    """Checks that `srun` works on the slurm cluster with the parametrized flags."""
+    output = remote.get_output(f"srun {allocation_flags} hostname")
+    assert output
+    assert "not found" not in output
+    print(output)
+
+
+@pytest.fixture
+def interactive_slurm_remote(remote: Remote, allocation_flags: str):
+    return SlurmRemote(
+        connection=remote.connection,
+        alloc=allocation_flags.split(),
+    )
+
+
+@requires_access_to_slurm_cluster
+def test_ensure_allocation_salloc(interactive_slurm_remote: SlurmRemote):
     # TODO: The SlurmRemote class is quite confusing: Is it a connection to the compute
     # node? Or is it a connection to the login node?
 
     # Actually runs the `salloc` command. Unclear if the connection to the compute node
     # is saved in `proc`..
-    data, proc = slurm_remote.ensure_allocation()
+    data, login_node_remote_runner = interactive_slurm_remote.ensure_allocation()
+    assert isinstance(login_node_remote_runner, fabric.runners.Remote)
+    print(data)
+    # WIP: Uncomment to use the WIP `get_output` equivalent without any additional
+    # classes. `Remote`
+    # run = make_get_output_fn(
+    #     login_node_remote_runner, interactive_slurm_remote.hostname
+    # )
 
-    hostname = data["node_name"]
+    run = interactive_slurm_remote.get_output
 
-    # {"node_name": node_name, "jobid": results["jobid"]}, proc
-    print(data, proc, hostname)
+    hostname_from_salloc_output_on_login_node = data["node_name"]
+
+    time.sleep(5)  # seems like squeue doesn't update quite fast enough sometimes.
+    squeue_output = run("squeue --me")
+    print("Got this output from squeue:", squeue_output)
+
+    # user = _run("whoami")
+    # hostname = _run("hostname")
+    assert hostname_from_salloc_output_on_login_node in squeue_output
 
 
-@requires_s_flag
-@requires_access_to_slurm_cluster
-def test_can_launch_srun(remote: Remote):
-    output = remote.get_output(
-        "srun --nodes=1 --ntasks=1 --cpus-per-task=1 --mem=1G --time=00:01:00 hostname"
+@pytest.fixture
+def batch_slurm_remote(remote: Remote, allocation_flags: str):
+    return SlurmRemote(
+        connection=remote.connection,
+        alloc=allocation_flags.split(),
+        persist=True,
     )
-    print(output)
-    # assert False, output
+
+
+@pytest.mark.timeout(60)
+@requires_access_to_slurm_cluster
+def test_ensure_allocation_sbatch(batch_slurm_remote: SlurmRemote):
+    # TODO: The SlurmRemote class is quite confusing: Is it a connection to the compute
+    # node? Or is it a connection to the login node?
+
+    job_data, login_node_remote_runner = batch_slurm_remote.ensure_allocation()
+    print(job_data, login_node_remote_runner)
+
+    assert isinstance(login_node_remote_runner, fabric.runners.Remote)
+
+    node_name_from_sbatch_extract = job_data["node_name"]
+    assert "jobid" in job_data
+    job_id_from_sbatch_extract = job_data["jobid"]
+
+    time.sleep(5)  # Let enough time for squeue to update.
+    squeue_output = batch_slurm_remote.run("squeue --me")
+    assert squeue_output
+    squeue_output = squeue_output.stdout.strip()
+    print(squeue_output)
+
+    assert node_name_from_sbatch_extract in squeue_output
+    assert job_id_from_sbatch_extract in squeue_output
+
+
+def _get_output(
+    login_node_remote_runner: fabric.runners.Remote, hostname: str, cmd: str, **kwargs
+):
+    """TODO: WIP: Trying to get rid of the need for the `SlurmRemote` class."""
+    result = login_node_remote_runner.run(
+        cmd,
+        # These two (echo and echo_format) do the same thing as the 'self.display'
+        # method of our `Remote` class.
+        echo=True,
+        echo_format=T.bold_cyan(f"({hostname})" + " $ {command}"),
+        in_stream=False,  # disable in_stream so tests work correctly.
+        hide="stdout",  # hide stdout because we'll be printing it in colour below.
+        **kwargs,
+    )
+    # TODO: Take a look at "stream watchers" in the fabric docs.
+    assert result
+    output = result.stdout.strip()
+    print(T.cyan(output))
+    return output
+
+
+def make_get_output_fn(login_node_remote_runner: fabric.runners.Remote, hostname: str):
+    return functools.partial(_get_output, login_node_remote_runner, hostname)
