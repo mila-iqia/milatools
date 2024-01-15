4fc6bce47a7bd51206be473724b40b81e5a9b237
Fabrice Normandin <normandf@mila.quebec>
2024-01-15 17:38:32 -0500

    Try to make the ssh-keygen work on Windows in CI
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>

--- milatools/cli/init_command.py
+++ milatools/cli/init_command.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import copy
 import difflib
 import json
+import shlex
 import shutil
 import subprocess
 import sys
@@ -238,7 +239,15 @@ def get_windows_home_path_in_wsl() -> Path:
 
 
 def create_ssh_keypair(ssh_private_key_path: Path, local: Local) -> None:
-    local.run("ssh-keygen", "-f", str(ssh_private_key_path), "-t", "rsa", "-N=''")
+    local.run(
+        "ssh-keygen",
+        "-f",
+        shlex.quote(str(ssh_private_key_path)),
+        "-t",
+        "rsa",
+        "-N",
+        "''",
+    )
 
 
 def setup_vscode_settings():
