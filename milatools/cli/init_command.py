f6836accaec5fe87f3939751eca9bb139022aa24
guillaumehu <guillaumehuguet2@gmail.com>
2024-11-07 20:36:42 -0500

    add ML hack

--- milatools/cli/init_command.py
+++ milatools/cli/init_command.py
@@ -435,10 +435,16 @@ def _copy_if_needed(linux_key_file: Path, windows_key_file: Path):
         shutil.copy2(src=linux_key_file, dst=windows_key_file)
 
 
+# def get_windows_home_path_in_wsl() -> Path:
+#     assert running_inside_WSL()
+#     windows_username = subprocess.getoutput("powershell.exe '$env:UserName'").strip()
+#     return Path(f"/mnt/c/Users/{windows_username}")
+
 def get_windows_home_path_in_wsl() -> Path:
     assert running_inside_WSL()
-    windows_username = subprocess.getoutput("powershell.exe '$env:UserName'").strip()
-    return Path(f"/mnt/c/Users/{windows_username}")
+    windows_username = subprocess.getoutput("powershell.exe '$env:HomePath'").strip()
+    windows_username = windows_username.replace('\\', '/')
+    return Path(f"/mnt/c/{windows_username}")
 
 
 def create_ssh_keypair(
