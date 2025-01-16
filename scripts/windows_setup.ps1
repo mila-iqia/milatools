1b3e1909f80e49093d1b720e6ac360a6ceabbcff
Fabrice Normandin <normandf@mila.quebec>
2025-01-16 10:25:38 -0500

    Update windows setup script
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>

--- scripts/windows_setup.ps1
+++ scripts/windows_setup.ps1
@@ -25,17 +25,18 @@ Write-Host "WSL setup complete. Restart your machine if required."
 # Install UV package manager from Astral.sh
 Write-Host "Installing UV package manager from Astral.sh..."
 # todo: Can probably just run `irm ... | iex` since we are already in a script?
-powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
+irm https://astral.sh/uv/install.ps1 | iex
 
 # Install Git if not already installed
 # TODO: This errors out, probably because TEMP isn't set.
 if (-not (Get-Command git -ErrorAction SilentlyContinue)) {
     Write-Host "Git not found. Installing..."
-    $gitInstallerUrl = "https://github.com/git-for-windows/git/releases/latest/download/Git-2.40.1-64-bit.exe"
-    $gitInstallerPath = "$env:TEMP\GitSetup.exe"
-    Invoke-WebRequest -Uri $gitInstallerUrl -OutFile $gitInstallerPath -UseBasicParsing
-    Start-Process -FilePath $gitInstallerPath -ArgumentList "/VERYSILENT" -Wait
-    Remove-Item $gitInstallerPath
+    winget install --id Git.Git -e --source winget
+    # $gitInstallerUrl = "https://github.com/git-for-windows/git/releases/latest/download/Git-2.40.1-64-bit.exe"
+    # $gitInstallerPath = "$env:TEMP\GitSetup.exe"
+    # Invoke-WebRequest -Uri $gitInstallerUrl -OutFile $gitInstallerPath -UseBasicParsing
+    # Start-Process -FilePath $gitInstallerPath -ArgumentList "/VERYSILENT" -Wait
+    # Remove-Item $gitInstallerPath
     Write-Host "Git installed successfully."
 } else {
     Write-Host "Git is already installed."
